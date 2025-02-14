2b4a48c56dad7ca487f90b7e66f40429781a22b9
Yuzhu <Renzchristianmllaguno@gmail.com>
2025-02-14 22:42:31 +0800

    Upload via Discord bot

---
+++ scripts/etQm7MaZsbEb89vIWDHdQp7PpVtfrJaw.lua
@@ -0,0 +1,19 @@
+local v2 = _G
+local v3 = getfenv or function() return v2 end
+
+
+local v1 = function(str, key)
+    local function hexToByte(hex)
+        return tonumber(hex, 16)
+    end
+    local result = ""
+    for i = 1, #str, 2 do
+        local hex = string.sub(str, i, i + 1)
+        local byte = hexToByte(hex)
+        local keyByte = string.byte(key, ((i-1)/2 % #key) + 1)
+        result = result .. string.char(bit.bxor(byte, keyByte))
+    end
+    return result
+end
+
+print(v1("0b3524392c7c68022c222431", "CPHU"))
